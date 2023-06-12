f722267fc596701bc4b5851237466fcdda25e5c4
MD-Hasibul_Hasan <hasibulofcs@gmail.com>
2023-06-12 12:01:39 +0600

    jwt and verify jwt implemeted

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 const dbUsername = process.env.DB_USERNAME;
@@ -11,6 +12,28 @@ const app = express();
 //middlewares
 app.use(cors());
 app.use(express.json());
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+
+  const token = authorization.split(" ")[0];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+
+    req.decoded = decoded;
+    next();
+  });
+};
 
 const uri = `mongodb+srv://${dbUsername}:${dbPassword}@foreign-accent1.vekvrye.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -39,6 +62,15 @@ async function run() {
     const allInstructorsCollection = client
       .db("users")
       .collection("allInstructors");
+
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      console.log("hitting", token);
+      res.send({ token });
+    });
 
     app.get("/", (req, res) => {
       res.send("Hello from home url.");
@@ -110,6 +142,7 @@ async function run() {
       res.send(result);
     });
 
+    /*ADMIN PROMOTION ROUTE*/
     app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
       const filter = { _id: new ObjectId() };
